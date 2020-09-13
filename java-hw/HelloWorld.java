114803765e705fccb45fddbccd5aa5882edff867
Ashok Kumar <ashok.kumar2@comviva.com>
2020-09-14 00:47:27 +0530

    docker 101 and 201 lab

---
+++ java-hw/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Comviva!");
+        System.out.println(" - java.version: " + System.getProperty("java.version"));
+        System.out.println(" - java.vm.version: " + System.getProperty("java.vm.version"));
+    }
+}
